b46773fd8b185afc4b56428abac3c5912fe9a306
razmes2009 <semin.02@mail.ru>
2024-08-22 14:00:22 +0300

    task manager

--- main.cpp
+++ main.cpp
@@ -1,39 +1,108 @@
 #include <iostream>
 #include <string>
-#include <ctime>
-#include <chrono>
+#include <vector>
+#include <iterator>
+
+#include <nlohmann/json.hpp>
 
 class Task
 {
 private:
     std::string title;
     std::string description;
-    std::time_t dueTime;
+    std::string dueTime;
     bool isCompleted{false};
 
 public:
     Task(const std::string &title, const std::string &description,
-         const std::time_t &dueTime);
-    ~Task();
+         const std::string &time);
+    ~Task() {};
+
+    const std::string &getTitle() const;
+    const std::string &getDescription() const;
+    const std::string &getTime() const;
+    const bool &getCompl() const;
 };
 
-Task::Task(const std::string &title, const std::string &description,
-           const std::time_t &dueTime)
+Task::Task(const std::string &_title, const std::string &_description,
+           const std::string &_dueTme)
 {
-    this->title = title;
-    this->description = description;
-    this->dueTime = dueTime;
+    this->title = _title;
+    this->description = _description;
+    this->dueTime = _dueTme;
 }
 
-Task::~Task()
+const std::string &Task::getTitle() const
 {
+    return this->title;
+};
+const std::string &Task::getDescription() const
+{
+    return this->description;
+};
+const std::string &Task::getTime() const
+{
+    return this->dueTime;
+};
+const bool &Task::getCompl() const
+{
+    return this->isCompleted;
+};
+
+class TaskManager
+{
+private:
+    std::vector<Task> tasks;
+
+public:
+    TaskManager();
+    ~TaskManager();
+
+    const void add_task(const Task &task);
+    const void delete_task(const size_t &id);
+    const void print_tasks() const;
+};
+
+TaskManager::TaskManager(/* args */)
+{
+}
+
+TaskManager::~TaskManager()
+{
+}
+
+const void TaskManager::add_task(const Task &task)
+{
+    tasks.push_back(task);
+}
+
+const void TaskManager::delete_task(const size_t &id)
+{
+    tasks.erase(tasks.begin() + id);
+}
+
+const void TaskManager::print_tasks() const
+{
+    for (auto &&task : this->tasks)
+    {
+        const std::string Compl = task.getCompl() ? "Completed" : "In progress";
+        std::cout << task.getTitle() << "\t|" << task.getDescription()
+                  << "\t|" << task.getTime() << "\t|" << Compl << std::endl;
+    }
 }
 
 int main(int argc, char const *argv[])
 {
-    auto timeNow = std::chrono::system_clock::now();
-    std::time_t time = std::chrono::system_clock::to_time_t(timeNow);
 
-    Task newTask = Task("test", "new test", time);
+    Task newTask = Task("test", "new test", "10.07.2023");
+    TaskManager manager = TaskManager();
+
+    manager.add_task(newTask);
+    manager.add_task(newTask);
+
+    manager.delete_task(1);
+
+    manager.print_tasks();
+
     return 0;
 }
